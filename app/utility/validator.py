150e989ab48c8ca7859b3b8a4a252e62562a9942
newzyz <nooknet102@gmail.com>
2020-09-06 13:43:15 +0700

    swe workshop ci/cd

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,4 +1,23 @@
+import re
+
+
 def validate_name(name):
+    name = str(name)
+    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    special = ["!", "@", "#", "$"]
+    if(name == ""):
+        return False
+
+    if " " in name:
+        return False
+
+    if(name.isalpha()):
+        return True
+
+    for s in name:
+        if (s in digits) or (s in special):
+            return False
+
     return True
 
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,34 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 class TestUtility(unittest.TestCase):
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("วิทวัส"))
+
+    def test_validate_name_with_valid_input_language(self):
+        self.assertEqual(True, validate_name("Wittawat"))
+
+    def test_validate_name_with_invalid_input_contain_string_digit(self):
+        self.assertEqual(False, validate_name("Wittawat50"))
+
+    def test_validate_name_with_invalid_input_contain_digit(self):
+        self.assertEqual(False, validate_name("100054545"))
+
+    def test_validate_name_with_invalid_input_contain_special(self):
+        self.assertEqual(False, validate_name("!#@///+-="))
+
+    def test_validate_name_with_invalid_input_contain_string_special(self):
+        self.assertEqual(False, validate_name("*#Wittawat"))
+
+    def test_validate_name_with_invalid_input_contain_number_special(self):
+        self.assertEqual(False, validate_name("*#5000"))
+
+    def test_validate_name_with_invalid_input_contain_string_number_special(self):
+        self.assertEqual(False, validate_name("Wittawat*#5000"))
+
+    def test_validate_name_with_empty(self):
+        self.assertEqual(False, validate_name(""))
+
+    def test_validate_name_with_space(self):
+        self.assertEqual(False, validate_name("chu tima"))
 
 
 if __name__ == '__main__':
